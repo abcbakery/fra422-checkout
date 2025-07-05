dd5cfd9f43f36560d27fd969dd85f855974a6b1a
abcbakery <gywls5545@naver.com>
2025-07-06 03:20:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,35 @@ const productData = {
     { name: "퍼퓸 하니 (50ML)", price: 119000, image: "image/perfume/HANI.png" }
   ],
   "TRAVEL-SIZE PERFUME": [
-    { name: "트래블 퍼퓸 1 (10ML)", price: 48000, image: "img/travel1.jpg" }
+    { name: "퍼퓸 원 (15ML)", price: 58000, image: "image/travel perfume/WON.png" },
+    { name: "퍼퓸 테오 (15ML)", price: 58000, image: "image/travel perfume/TEO.png" },
+    { name: "퍼퓸 엘 (15ML)", price: 58000, image: "image/travel perfume/EL.png" },
+    { name: "퍼퓸 킴 (15ML)", price: 58000, image: "image/travel perfume/KIM.png" },
+    { name: "퍼퓸 하니 (15ML)", price: 58000, image: "image/travel perfume/HANI.png" }
+  ],
+  "ANTI-WRINKLE PERFUME HAND CREAM": [
+    { name: "퍼퓸 핸드크림 엘 (30ML)", price: 23000, image: "image/handcream/EL.png" },
+    { name: "퍼퓸 핸드크림 킴 (30ML)", price: 23000, image: "image/handcream/KIM.png" },
+    { name: "퍼퓸 핸드크림 하니 (30ML)", price: 23000, image: "image/handcream/HANI.png" }
+  ],
+  "SACHET": [
+    { name: "퍼퓸 샤쉐 원 (20G)", price: 29000, image: "image/sachet/WON.png" },
+    { name: "퍼퓸 샤쉐 킴 (20G)", price: 29000, image: "image/sachet/KIM.png" }
+  ],
+  "CONCENTRATED DIFFUSER": [
+    { name: "베르가못 (15ML)", price: 24000, image: "image/diffuser/bergamot.png" },
+    { name: "시더우드 (15ML)", price: 19000, image: "image/diffuser/cedarwood.png" },
+    { name: "로즈 제라늄 (15ML)", price: 39000, image: "image/diffuser/geranium.png" },
+    { name: "그레이프프룻 (15ML)", price: 19000, image: "image/diffuser/grapefruit.png" },
+    { name: "라벤더 (15ML)", price: 29000, image: "image/diffuser/lavender.png" },
+    { name: "레몬 (15ML)", price: 19000, image: "image/diffuser/lemon.png" },
+    { name: "네롤리 (15ML)", price: 55000, image: "image/diffuser/neroli.png" },
+    { name: "파인 (15ML)", price: 19000, image: "image/diffuser/pine.png" },
+    { name: "로즈 (15ML)", price: 159000, image: "image/diffuser/rose.png" },
+    { name: "로즈마리 (15ML)", price: 19000, image: "image/diffuser/rosemary.png" },
+    { name: "지속가능한, 422로즈우드 (15ML)", price: 42000, image: "image/diffuser/422 rosewood.png" },
+    { name: "지속가능한, 422샌달우드 (15ML)", price: 42000, image: "image/diffuser/422샌달우드.png" },
+    { name: "지속가능한, 422가이악 (15ML)", price: 42000, image: "image/diffuser/422 guaiac.png" }
   ]
 };
 
